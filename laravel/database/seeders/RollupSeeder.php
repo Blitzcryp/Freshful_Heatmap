9ac002d682735080ab8622fd3ecc6078d2f64e19
B̴l̷i̷t̶x̷ <59416533+Blitzcryp@users.noreply.github.com>
2022-03-13 22:39:51 +0200

    Better seeders for testing

--- laravel/database/seeders/RollupSeeder.php
+++ laravel/database/seeders/RollupSeeder.php
@@ -12,6 +12,22 @@ class RollupSeeder extends Seeder
     public function run()
     {
         $insertData = collect([]);
+
+        foreach([LinksTypeEnums::HOMEPAGE, LinksTypeEnums::STATIC_PAGE, LinksTypeEnums::PRODUCT] as $linkType){
+            $insertData->push([
+                "uid" => 3,
+                "link" => "random-link-for-" . $linkType,
+                "linkType" => $linkType,
+                "timestamp" => Carbon::now()->toDateTimeString()
+            ]);
+            $insertData->push([
+                "uid" => 4,
+                "link" => "random-link-for-" . $linkType,
+                "linkType" => $linkType,
+                "timestamp" => Carbon::now()->toDateTimeString()
+            ]);
+        }
+
         foreach(LinksTypeEnums::cases() as $linkType){
             $insertData->push([
                 "uid" => 1,
@@ -21,6 +37,12 @@ class RollupSeeder extends Seeder
             ]);
             $insertData->push([
                 "uid" => 2,
+                "link" => "random-link-for-" . $linkType,
+                "linkType" => $linkType,
+                "timestamp" => Carbon::now()->toDateTimeString()
+            ]);
+            $insertData->push([
+                "uid" => 5,
                 "link" => "random-link-for-" . $linkType,
                 "linkType" => $linkType,
                 "timestamp" => Carbon::now()->toDateTimeString()
--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -30,6 +30,37 @@ class UserSeeder extends Seeder
                 "timezone" => "Europe/Athens",
                 "password_hash" => $this->hasher->make("321alorap"),
             ],
+            [
+                "id" => 3,
+                "fname" => "Coman Test1",
+                "lname" => "Cadius",
+                "last_login_country" => "GB",
+                "email" => "comancosmin_crusade@gmail.com",
+                "email_verified_at" => Carbon::yesterday()->toDateTime(),
+                "timezone" => "Europe/Athens",
+                "password_hash" => $this->hasher->make("321alorap"),
+            ],
+            [
+                "id" => 4,
+                "fname" => "Coman Test2",
+                "lname" => "Ezekiel",
+                "last_login_country" => "FR",
+                "email" => "comancosmin_warhammer@gmail.com",
+                "email_verified_at" => Carbon::yesterday()->toDateTime(),
+                "timezone" => "Europe/Athens",
+                "password_hash" => $this->hasher->make("321alorap"),
+            ],
+            [
+                "id" => 5,
+                "fname" => "Coman Test3",
+                "lname" => "Diablo",
+                "last_login_country" => "FR",
+                "email" => "comancosmin_warhammer@hell.com",
+                "email_verified_at" => Carbon::yesterday()->toDateTime(),
+                "timezone" => "Europe/Athens",
+                "password_hash" => $this->hasher->make("321alorap"),
+            ],
+
         ];
 
         $this->databaseManager
